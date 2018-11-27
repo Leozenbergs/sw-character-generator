a6a55af101dffe1ddd900bc474adc2f296f3b0e6
Leonardo Abreu <leoabreu14@gmail.com>
2018-11-27 00:00:16 -0200

    Returning to pattern for building local

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/SW-Character-Generator/dist/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
