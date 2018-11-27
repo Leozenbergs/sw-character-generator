d579056786e7c8672a536555beb80b6381a209fe
Leonardo Abreu <leoabreu14@gmail.com>
2018-11-26 23:50:12 -0200

    Trying att webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'SW-Character-Generator/dist/',
     filename: 'build.js'
   },
   module: {
