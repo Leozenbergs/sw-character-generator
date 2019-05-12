f25a01e7530e163fd0c938797030184b0516a0a4
Leonardo Rodrigues <leoabreu14@gmail.com>
2019-05-12 01:57:59 -0300

    Fix server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, '/')));
-const port = process.env.PORT || 5000;
-app.listen(port);
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, '/')))
+
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
